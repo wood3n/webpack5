89eb2d8da6d5937ad813c1fb8249010675f77508
王凯 <wangkka1@163.com>
2020-11-28 18:42:53 +0800

    测试webpack5分入口打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     main: {
       import: "./src/index.jsx",
-      filename: "main.[contentHash].js",
+      filename: "main.[contenthash:8].js",
       dependOn: "react-vendors",
     },
     test: {
       import: "./src/test.jsx",
-      filename: "test.[contentHash].js",
+      filename: "test.[contenthash:8].js",
       dependOn: "react-vendors",
     },
     "react-vendors": ["react", "react-dom"],
   },
+  // entry: {
+  //   main: "./src/index.jsx",
+  //   test: "./src/test.jsx",
+  // },
   output: {
-    filename: "[name].[contentHash].js",
+    filename: "[name].[contenthash:8].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -35,5 +40,53 @@ module.exports = {
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin(
+      Object.assign(
+        {},
+        {
+          inject: true,
+          template: "index.html",
+          chunks: ["main", "react-vendors"],
+          filename: "main.html",
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+          },
+        }
+      )
+    ),
+    new HtmlWebpackPlugin(
+      Object.assign(
+        {},
+        {
+          inject: true,
+          template: "index.html",
+          chunks: ["test", "react-vendors"],
+          filename: "test.html",
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+          },
+        }
+      )
+    ),
+    new CleanWebpackPlugin(),
+  ],
 };
